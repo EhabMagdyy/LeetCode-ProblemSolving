8a4a5ff56b7714a67ed3eb66d349e0f94a95023f
EhabMagdyy <ehabmagdy0044@gmail.com>
2024-12-05 00:02:06 +0200

    22

---
+++ 22.generate-parentheses.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=22 lang=cpp
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    std::vector<std::string> generateParenthesis(int n)
+    {
+        std::vector<std::string> result = {};
+        
+    }
+};
+// @lc code=end
